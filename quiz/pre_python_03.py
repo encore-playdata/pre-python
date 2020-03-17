c0e3b97a3a2e2e89d239f24ba013b4cc92c6b67e
daljaru <2heri@naver.com>
2020-03-17 14:46:22 +0900

    answer_quiz_03

--- quiz/pre_python_03.py
+++ quiz/pre_python_03.py
@@ -11,3 +11,36 @@
 첫 번째(두 번째) 참가자의 승리입니다. or 비겼습니다.
 
 """
+
+import random;
+
+def checkAndPrint(enter):
+        if (enter == ''):
+            randNum = random.randint(1, 6)
+            print(randNum)
+            return randNum
+        else:
+            print("엔터키만 입력해야합니다.")
+            return 0
+
+
+while True:
+    enter = input("첫번째 참가자 엔터키를 눌러 주사위를 던져 주세요 : ")
+    firNum = checkAndPrint(enter)
+    if firNum != 0:
+        break
+
+while True:
+    enter = input("두번째 참가자 엔터키를 눌러 주사위를 던져 주세요 : ")
+    secNum = checkAndPrint(enter)
+    if secNum != 0:
+        break
+
+
+if(firNum > secNum):
+    print("첫번째 참가자의 승리입니다.")
+elif(firNum < secNum):
+    print("두번째 참가자의 승리입니다.")
+else:
+    print("비겼습니다.")
+
