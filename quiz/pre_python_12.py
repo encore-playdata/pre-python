694df98803460e3d2ac90a35c3295fd567973be2
daljaru <2heri@naver.com>
2020-03-18 11:58:13 +0900

    answer_quiz_07_15

--- quiz/pre_python_12.py
+++ quiz/pre_python_12.py
@@ -1,4 +1,4 @@
-"""12. 리스트 메서드 중 하나를 이용하여 아래의 리스트를 
+"""12. 리스트 메서드 중 하나를 이용하여 아래의 리스트를
 ABCD 순으로 정렬하시오.
 
 namelist = ['Mary', 'Sams', 'Aimy', 'Tom', 'Michale', 'Bob', 'Kelly']
@@ -10,3 +10,7 @@ print(namelist)
 <출력>
 ['Aimy', 'Bob', 'Kelly', 'Mary', 'Michale', 'Sams', 'Tom']
 """
+
+namelist = ['Mary', 'Sams', 'Aimy', 'Tom', 'Michale', 'Bob', 'Kelly']
+namelist = sorted(namelist)
+print(namelist)
