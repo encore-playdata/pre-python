59428befea67eee99c15bb62421ba1fa9d9a4c4c
Jay <skyla15@nate.com>
2020-06-18 01:50:05 +0900

    'update'

--- quiz/quiz2_3.py
+++ quiz/quiz2_3.py
@@ -22,3 +22,44 @@ card.print()
 잔액이 부족합니다
 잔액이 9000원 입니다.
 '''
+
+import math
+
+class Card(object):
+    __slots__ = '_balance', '_withdrawal_record'
+
+    def __init__(self):
+        self._balance = 0
+        self._withdrawal_record = list(tuple())
+
+    def charge(self, cost):
+        self._balance += cost
+
+    def check_dc(self, charge, used_for):
+        if used_for == '영화관':
+            return math.ceil(charge*0.8)
+
+    def consume(self, charge, used_for = None ):
+        used_for = self.check_dc(charge, used_for)
+        if charge > self._balance:
+            print('잔액이 부족합니다.')
+            return
+        self._withdrawal_record.append((used_for, charge))
+        print('{}에서 {}원 사용했습니다.'.format(used_for, charge))
+
+    def print(self):
+        print('--잔액이 {}원입니다--'.format(self._balance))
+        for i, c in enumerate(self._withdrawal_record):
+            print('{} {} : {}'.format(i, c[0], c[1]))
+
+
+def main():
+    card = Card()
+    card.charge(20000)
+    card.consume(3000,'마트')
+    card.consume(10000,'영화관')
+    card.consume(13000,'마트')
+    card.print()
+
+if __name__ == '__main__':
+    main()
--- quiz/quiz2_4.py
+++ quiz/quiz2_4.py
@@ -23,3 +23,63 @@ multi_card.print()
 교통에서 1800.0원을 사용했습니다.
 잔액이 5700.0원 입니다
 '''
+
+import math
+
+class Movie_card():
+    def __init__(self):
+        pass
+
+    def movie_discount(self, charge):
+        return math.ceil(charge * 0.9)
+
+
+class Mart_card():
+    def __init__(self):
+        pass
+
+    def mart_discount(self, charge):
+        return math.ceil(charge*0.9)
+
+
+class Card(Movie_card, Mart_card):
+
+    def __init__(self):
+        self._balance = 0
+        self._withdrawal_record = list(tuple())
+
+    def charge(self, deposit):
+        self._balance += deposit
+
+    def check_dc(self, charge, used_for):
+        if used_for == '영화관':
+            return math.ceil(super().mart_discount(charge))
+        elif used_for == '마트':
+            return math.ceil(super().movie_discount(charge))
+        else:
+            return charge
+
+    def consume(self, charge, used_for):
+        charge = self.check_dc(charge, used_for)
+        if charge > self._balance:
+            print('잔액이 부족합니다.')
+            return
+        self._withdrawal_record.append((used_for, charge))
+        print('{}에서 {}원 사용했습니다.'.format(used_for, charge))
+        self._balance -= charge
+
+    def print(self):
+        print('잔액이 {}원입니다'.format(self._balance))
+        for i, c in enumerate(self._withdrawal_record):
+            print('{} {} : {}'.format(i, c[0], c[1]))
+
+
+multi_card=Card()
+multi_card.charge(20000)
+multi_card.consume(5000,'마트')
+multi_card.consume(10000,'영화관')
+multi_card.consume(10000,'영화관')
+multi_card.consume(10000,'영화관')
+multi_card.consume(2000,'교통')
+multi_card.print()
+
